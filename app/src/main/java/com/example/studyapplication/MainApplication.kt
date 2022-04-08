ef2422b73667766e45dc234c520c062c272a8b9a
Kevin-Mattos <kevinmmattos@gmail.com>
2022-04-08 10:13:03 -0300

    feat: add logger and context to koin

--- app/src/main/java/com/example/studyapplication/MainApplication.kt
+++ app/src/main/java/com/example/studyapplication/MainApplication.kt
@@ -2,6 +2,8 @@ package com.example.studyapplication
 
 import android.app.Application
 import com.example.coreui.di.coreUiModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MainApplication: Application() {
@@ -9,6 +11,8 @@ class MainApplication: Application() {
 	override fun onCreate() {
 		super.onCreate()
 		startKoin {
+			androidLogger()
+			androidContext(this@MainApplication)
 			modules(
 				coreUiModule
 			)
